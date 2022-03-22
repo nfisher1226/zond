2598cbfba284e581f3f285158b58a495a8a264d3
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-03-22 09:51:40 -0400

    Added README

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//! Running concurrently to NASA's project Mercury, the Soviet *Vostok*, or if you
+//! prefer the original Russian, *Восток* project was a human spaceflight program
+//! with the goal of putting the first human into low earth orbit and returning them
+//! safely. Yuri Gagarin in fact became the first human to do so on April 12th, 1961.
+//!
+//! In this context, Vostok is a static gemini capsule generator, think static site
+//! generator like Hugo or Jekyll but for [Project Gemini](https://gemini.circumlunar.space).
+//!
+//! ## Status
+//! This software is currently under construction and does not yet perform it's
+//! intended task. Please check back later.
 pub mod command;
 pub mod config;
 pub mod content;
