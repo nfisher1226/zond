a4033fdbd2dd76784b6773a655c048afed8bafc3
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-04-04 09:51:38 -0400

    Finish migration to xtask packager

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,23 +1,51 @@
-#[cfg(any(feature = "completions", feature = "manpage"))]
-use std::env;
-#[cfg(feature = "completions")]
 use {
     clap_complete::{generate_to, shells},
-};
-
-#[cfg(feature = "manpage")]
-use {
     clap_mangen::Man,
-    std::path::PathBuf,
+    std::{env, error::Error, fs, path::PathBuf, process},
 };
 
-use std::io::Error;
+include!("../../src/cli.rs");
 
-#[cfg(any(feature = "completions", feature = "manpage"))]
-include!("src/cli.rs");
+fn completions() -> Result<(), Box<dyn Error>> {
+    println!("Generating completions:");
+    let mut cmd = build();
+    let outdir: PathBuf = ["target", "dist", "share", "bash-completion", "completions"]
+        .iter()
+        .collect();
+    if !outdir.exists() {
+        fs::create_dir_all(&outdir)?;
+    }
+    let path = generate_to(shells::Bash, &mut cmd, "zond", outdir)?;
+    println!("    {}", path.display());
+    let outdir: PathBuf = ["target", "dist", "share", "zsh", "site-functions"]
+        .iter()
+        .collect();
+    if !outdir.exists() {
+        fs::create_dir_all(&outdir)?;
+    }
+    let path = generate_to(shells::Zsh, &mut cmd, "zond", outdir)?;
+    println!("    {}", path.display());
+    let outdir: PathBuf = ["target", "dist", "share", "fish", "completions"]
+        .iter()
+        .collect();
+    if !outdir.exists() {
+        fs::create_dir_all(&outdir)?;
+    }
+    let path = generate_to(shells::Fish, &mut cmd, "zond", outdir.to_path_buf())?;
+    println!("    {}", path.display());
+    // Disabling this for now because I don't know where powershell looks for completions
+    let outdir: PathBuf = ["target", "dist", "share", "pwsh", "completions"]
+        .iter()
+        .collect();
+    if !outdir.exists() {
+        fs::create_dir_all(&outdir)?;
+    }
+    let path = generate_to(shells::PowerShell, &mut cmd, "zond", outdir.to_path_buf())?;
+    println!("    {}", path.display());
+    Ok(())
+}
 
-#[cfg(feature = "manpage")]
-fn build_man(cmd: &str) -> Result<(), Error> {
+fn manpage(cmd: &str) -> Result<(), Box<dyn Error>> {
     let (fname, cmd) = match cmd {
         "zond-build" => ("zond-build.1", build_build()),
         "zond-init" => ("zond-init.1", build_init()),
@@ -25,39 +53,47 @@ fn build_man(cmd: &str) -> Result<(), Error> {
         "zond-post" => ("zond-post.1", build_post()),
         _ => ("zond.1", build()),
     };
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
-    };
-    let file: PathBuf = [outdir.to_str().unwrap(), fname].iter().collect();
+    let outdir: PathBuf = ["target", "dist", "share", "man", "man1"].iter().collect();
+    if !outdir.exists() {
+        fs::create_dir_all(&outdir)?;
+    }
+    let mut outfile = outdir;
+    outfile.push(fname);
     let man = Man::new(cmd);
     let mut buffer: Vec<u8> = Vec::new();
     man.render(&mut buffer)?;
-    std::fs::write(file, buffer)?;
+    std::fs::write(&outfile, buffer)?;
+    println!("    {}", outfile.display());
     Ok(())
 }
 
-fn main() -> Result<(), Error> {
-    #[cfg(feature = "completions")]
-    {
-        let outdir = match env::var_os("OUT_DIR") {
-            None => return Ok(()),
-            Some(outdir) => outdir,
-        };
-        let mut cmd = build();
-        let _path = generate_to(shells::Bash, &mut cmd, "zond", outdir.clone())?;
-        let _path = generate_to(shells::Zsh, &mut cmd, "zond", outdir.clone())?;
-        let _path = generate_to(shells::Fish, &mut cmd, "zond", outdir.clone())?;
-        let _path = generate_to(shells::PowerShell, &mut cmd, "zond", outdir.clone())?;
-        println!(
-            "cargo:warning=Shell completions have been saved in: {:?}",
-            outdir
-        );
-    }
-    #[cfg(feature = "manpage")]
+fn manpages() -> Result<(), Box<dyn Error>> {
+    println!("Generating man pages:");
     ["zond", "zond-build", "zond-init", "zond-page", "zond-post"]
         .iter()
-        .try_for_each(|x| build_man(x))?;
-    println!("cargo:rerun-if-changed=build.rs");
+        .try_for_each(|cmd| manpage(cmd))?;
+    Ok(())
+}
+
+fn usage() {
+    println!("Usage: xtask dist");
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        usage();
+        process::exit(0);
+    }
+    if &args[1] == "dist" {
+        let outdir: PathBuf = ["target", "dist"].iter().collect();
+        if outdir.exists() {
+            fs::remove_dir_all(&outdir)?;
+        }
+        completions()?;
+        manpages()?;
+    } else {
+        usage();
+    }
     Ok(())
 }
