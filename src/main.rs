7eed3a74314a235b288de793215d8e8c09285051
Nathan Fisher <jeang3nie@hitchhiker-linux.org>
2022-11-13 01:29:15 -0500

    Add /usr/local to language search paths

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use gettextrs::*;
 
 fn main() -> Result<(), zond::Error> {
-    textdomain("zond")?;
+    TextDomain::new("zond")
+        .push("/usr/local/share")
+        .init()
+        .expect("Error initializing locale");
     bind_textdomain_codeset("zond", "UTF-8")?;
     let matches = zond::cli::zond().get_matches();
     zond::command::run(&matches)?;
